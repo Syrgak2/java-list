fde917398ec74b8a9363fb04fa7b160be5934da3
syrga <kary6ekov.s@gmail.com>
2023-10-31 12:55:46 +0100

    create project

---
+++ src/main/java/skyPro/homework/EmployeeAccountingApplication.java
@@ -0,0 +1,13 @@
+package skyPro.homework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeAccountingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeAccountingApplication.class, args);
+	}
+
+}
